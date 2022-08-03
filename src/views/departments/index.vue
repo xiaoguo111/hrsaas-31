967b532d384ffd7f8728006fc4c75c693ba44eac
xiaoguo <348383021@qq.com>
2022-08-03 16:13:45 +0800

    创建 departments

---
+++ src/views/departments/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="dashboard-container">
+    <div class="app-container">
+      <el-card class="box-card">
+        <div v-for="o in 4" :key="o" class="text item">
+          {{ "列表内容 " + o }}
+        </div>
+      </el-card>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+
+  created() {},
+
+  methods: {},
+};
+</script>
+
+<style scoped lang="less"></style>
